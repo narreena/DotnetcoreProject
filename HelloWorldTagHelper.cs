32fd635d76ef3ac0880b0fb3832f81af5f6fadd6
Naresh Kumar <95981159+narreena@users.noreply.github.com>
2022-02-05 13:08:17 +0530

    Add first dotnet project

---
+++ HelloWorldTagHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace MyFirstCoreProject
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using System.Text;
+    [HtmlTargetElement("my-first-tag-helper")]
+    public class HelloWorldTagHelper : TagHelper
+    {
+        public String Name { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            this.Name = "Naresh Custom Tag";
+            output.TagName = "CustomTagHelper";
+            output.TagMode= TagMode.StartTagAndEndTag;
+            var sbstr = new StringBuilder();
+            sbstr.AppendFormat("<span>Hello! My Name is {0}</span>", this.Name);
+            output.PreContent.SetHtmlContent(sbstr.ToString());
+        }
+    }
+}
